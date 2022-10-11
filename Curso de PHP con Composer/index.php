b7c57bdb003f5a7a386d58789dca9c4d27f0a4cf
David Olvera <licdavidob@gmail.com>
2022-10-10 20:52:34 -0500

    Iniciando curso de HTML y CSS

--- Curso de PHP con Composer/index.php
+++ Curso de PHP con Composer/index.php
@@ -1,4 +1,4 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-echo Text\Format::upperText("pablito");
+echo upper("kilo");
 ?>
---
+++ Curso de PHP con Composer/src/helpers.php
@@ -0,0 +1,6 @@
+<?php
+function upper($Valor){
+    return Text\Format::upperText($Valor);
+}
+
+?>
