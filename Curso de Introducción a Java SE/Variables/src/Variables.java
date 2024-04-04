156873f4aa8bab7378271a1c5dcb5e7e50d3e0e8
David OLvera <licdavidob@gmail.com>
2024-04-04 10:57:48 -0600

    Curso de Almacenamiento GCP

--- Curso de Introducción a Java SE/Variables/src/Variables.java
+++ Curso de Introducción a Java SE/Variables/src/Variables.java
@@ -1,2 +1,9 @@
 public class Variables {
+    public static void main(String[] args) {
+        // Declare a variable
+        int myNumber = 10;
+
+        //Imprimir variable
+        System.out.println(myNumber);
+    }
 }
